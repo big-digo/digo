eacabcfa84b2b02a6c9880e1fef4edf80443a71d
pyaly <991526442@qq.com>
2017-06-02 17:41:47 +0800

    commit test

--- src/main/java/com/cetc/bigdata/analysis/web/controller/DashboardController.java
+++ src/main/java/com/cetc/bigdata/analysis/web/controller/DashboardController.java
@@ -2,10 +2,12 @@ package com.cetc.bigdata.analysis.web.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 
 import com.alibaba.fastjson.JSONObject;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/dashboard")
